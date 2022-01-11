6a7a88be09f9eb642410a72b0d066e57d03d86b3
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-01-11 10:14:42 -0300

    refactor: :recycle: change project name and url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAQHCnj2tftpwxbuHFta8ptq5lO8pLiZEQ",
-    authDomain: "firestarter-rod.firebaseapp.com",
-    projectId: "firestarter-rod",
-    storageBucket: "firestarter-rod.appspot.com",
-    messagingSenderId: "972057195471",
-    appId: "1:972057195471:web:0dcfb4741196d3efab58ba"
-  }
+    apiKey: 'AIzaSyAv5vMfb-FkDpArqA7fTtGR7E5kXsS4jbg',
+    authDomain: 'boards-rod.firebaseapp.com',
+    projectId: 'boards-rod',
+    storageBucket: 'boards-rod.appspot.com',
+    messagingSenderId: '1092892128002',
+    appId: '1:1092892128002:web:18aa5bd6518ab38773c81b',
+  },
 };
